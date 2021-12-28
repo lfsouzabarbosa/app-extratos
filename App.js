9a0e15437c539e5e0c366cf2dc6114d5d764b9c1
lfsouzabarbosa <36238862+lfsouzabarbosa@users.noreply.github.com>
2021-12-27 22:43:15 -0300

    Update App.js

--- App.js
+++ App.js
@@ -235,8 +235,9 @@ apiTK.get('/', (req, res) => {
   const puppeteer = require('puppeteer');
   (async () => {
       const browser = await puppeteer.launch({
-  executablePath: '/usr/bin/chromium-browser'
-})
+      headless: false,
+      args: ['--no-sandbox']
+      });
       const page = await browser.newPage();
       await page.goto('https://canalconsorciado.bradesco.com.br/valor-receber');
       await page.type('input[name=grupo]', grupo, { delay: 100 }); // '4830'
