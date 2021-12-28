f4be6f5fa011ac99ee299e28e3a3e9bdd8a4eba2
lfsouzabarbosa <36238862+lfsouzabarbosa@users.noreply.github.com>
2021-12-27 23:41:20 -0300

    Update App.js

--- App.js
+++ App.js
@@ -240,11 +240,11 @@ apiTK.get('/', (req, res) => {
       })
       const page = await browser.newPage();
       await page.goto('https://canalconsorciado.bradesco.com.br/valor-receber');
-      await page.$('input[name=grupo]', grupo, { delay: 100 }); // '4830'
-      await page.$('input[name=cota]', cota, { delay: 100 }); // '23'
-      await page.$('input[name=inscricaoNacional]', documento, { delay: 100 }); // '51941163653'
-      await page.$('input[name=numeroContrato]', contrato, { delay: 100 }); // '109242563'
-      await page.$('button[type="submit"]');
+      await page.$$('input[name=grupo]', grupo, { delay: 100 }); // '4830'
+      await page.$$('input[name=cota]', cota, { delay: 100 }); // '23'
+      await page.$$('input[name=inscricaoNacional]', documento, { delay: 100 }); // '51941163653'
+      await page.$$('input[name=numeroContrato]', contrato, { delay: 100 }); // '109242563'
+      await page.$$('button[type="submit"]');
 
       page.on('request', async (request) => {
           //console.log('<<', request.url())
