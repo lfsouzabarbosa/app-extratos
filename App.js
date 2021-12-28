0a8ff6c2cb77a076411673f06a6d92487ef68473
lfsouzabarbosa <36238862+lfsouzabarbosa@users.noreply.github.com>
2021-12-28 00:09:19 -0300

    Update App.js

--- App.js
+++ App.js
@@ -240,9 +240,13 @@ apiTK.get('/', (req, res) => {
       })
       const page = await browser.newPage();
       await page.goto('https://canalconsorciado.bradesco.com.br/valor-receber');
+      await page.waitForSelector('input[name=grupo]');
       await page.type('input[name=grupo]', grupo, { delay: 100 }); // '4830'
+      await page.waitForSelector('input[name=cota]');
       await page.type('input[name=cota]', cota, { delay: 100 }); // '23'
+      await page.waitForSelector('input[name=inscricaoNacional]');
       await page.type('input[name=inscricaoNacional]', documento, { delay: 100 }); // '51941163653'
+      await page.waitForSelector('input[name=numeroContrato]');
       await page.type('input[name=numeroContrato]', contrato, { delay: 100 }); // '109242563'
       await page.click('button[type="submit"]');
 
